cbae3e5f2ae3e0b9cf547097e5e66ab6837b0c7f
Mike Brocchi <mbrocchi@gmail.com>
2017-08-01 14:33:53 -0400

    WIP

--- angular-app/files/e2e/app.e2e-spec.ts
+++ angular-app/files/e2e/app.e2e-spec.ts
@@ -9,6 +9,6 @@ describe('<%= utils.dasherize(name) %> App', () => {
 
   it('should display message saying app works', () => {
     page.navigateTo();
-    expect(page.getParagraphText()).toEqual('<%= prefix %> works!');
+    expect(page.getParagraphText()).toEqual('Welcome to <%= prefix %>!');
   });
 });
